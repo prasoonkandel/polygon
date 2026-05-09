0e51505d6b4be3725eeee737afd001c8dfd3adc4
Prasoon Kandel <prasoonkandel1029@gmail.com>
2026-05-09 11:12:04 +0545

    feat: add some content to web

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  75,
+  50,
   window.innerWidth / window.innerHeight,
   0.1,
   1000,
@@ -15,7 +15,7 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.set(35, 25, 35);
+camera.position.set(35, 10, 35);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
