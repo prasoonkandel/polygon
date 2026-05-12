9d821eb97d1f48b3f80a897317610dd5fa972f1b
Prasoon Kandel <prasoonkandel1029@gmail.com>
2026-05-12 19:38:01 +0545

    fix: removing light and grid helpers

--- main.js
+++ main.js
@@ -32,9 +32,9 @@ pointLight.intensity = 100;
 pointLight.distance = 500;
 scene.add(pointLight, ambientLight);
 
-const lightHelper = new THREE.PointLightHelper(pointLight);
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(lightHelper, gridHelper);
+// const lightHelper = new THREE.PointLightHelper(pointLight);
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(lightHelper, gridHelper);
 
 scene.add(torus);
 let torusAlive = true;
